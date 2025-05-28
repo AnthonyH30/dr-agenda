3fc92d20ae3429f36fd95a17618fcb39f0028367
anthonyh30 <anthonyharisson30@gmail.com>
2025-05-28 02:58:01 -0300

    feat: add clinic form page

---
+++ src/app/clinic-form/page.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+
+import ClinicForm from "./components/form";
+
+export default function ClinicFormPage() {
+  return (
+    <Dialog open>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Adicionar clínica</DialogTitle>
+          <DialogDescription>Adicione uma para continuar.</DialogDescription>
+        </DialogHeader>
+        <ClinicForm />
+      </DialogContent>
+    </Dialog>
+  );
+}
